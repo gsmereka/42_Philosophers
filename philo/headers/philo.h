af6a945d213230545dd669556d1c10088eb70d2d
gsmereka <gabrielsmereka200@gmail.com>
2023-05-02 21:57:55 -0300

    att headers

--- philo/headers/philo.h
+++ philo/headers/philo.h
@@ -6,11 +6,31 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 17:27:17 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/05/02 17:28:59 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/05/02 21:09:32 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 # include "structs.h"
+
+// FUNCTIONS: LIBRARIES
+// memset: string.h
+// printf: stdio.h
+// malloc, free: stdlib.h
+// write: unistd.h
+// usleep: unistd.h
+// gettimeofday: sys/time.h
+// pthread_create, pthread_detach,
+// pthread_join, pthread_mutex_init, 
+// pthread_mutex_destroy, pthread_mutex_lock,
+// pthread_mutex_unlock: pthread.hs
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
 #endif
