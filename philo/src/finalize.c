990d051eff5165d8966eb86245f2629433da8b0d
gsmereka <gabrielsmereka200@gmail.com>
2023-05-06 17:35:11 -0300

    att free stuff

--- philo/src/finalize.c
+++ philo/src/finalize.c
@@ -6,13 +6,14 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 21:22:12 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/05/06 17:23:24 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/05/06 17:33:27 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/philo.h"
 
 static void	free_data(t_data *data);
+void	free_generic_array(void **array, int limit);
 void	free_array_list(void **list);
 
 void	finalize(t_data *data)
@@ -30,16 +31,16 @@ void	exit_error(int status, char *msg, t_data *data)
 
 static void	free_data(t_data *data)
 {
-	if (data->config)
-		free(data->config);
 	if (data->philosophers)
-		free_array_list((void **)data->philosophers);
+		free_generic_array((void **)data->philosophers, data->config->number_of_philosophers);
 	if (data->philo_threads)
-		free_array_list((void **)data->philo_threads);
+		free_generic_array((void **)data->philo_threads, data->config->number_of_philosophers);
 	if (data->forks)
-		free_array_list((void **)data->forks);
+		free_generic_array((void **)data->forks, data->config->number_of_philosophers);
 	if (data->mutex)
 		free(data->mutex);
+	if (data->config)
+		free(data->config);
 }
 
 void	free_array_list(void **list)
@@ -53,4 +54,17 @@ void	free_array_list(void **list)
 		i++;
 	}
 	free(list);
+}
+
+void	free_generic_array(void **array, int limit)
+{
+	int	i;
+
+	i = 0;
+	while (i < limit)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
 }
