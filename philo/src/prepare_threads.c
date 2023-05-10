1cc0d5278796a2deff47b45a4f58c28111ae5537
gsmereka <gabrielsmereka200@gmail.com>
2023-05-10 17:18:34 -0300

    att init mutexes

--- philo/src/prepare_threads.c
+++ philo/src/prepare_threads.c
@@ -6,41 +6,26 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 14:07:13 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/05/09 20:08:27 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/05/10 17:18:09 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/philo.h"
 
-int	get_start_time(t_data *data)
-{
-	struct timeval	time;
-
-	gettimeofday(&time, NULL);
-	data->start_time = (time.tv_sec * 1000) + (time.tv_usec / 1000);
-	return (0);
-}
+static void	prepare_mutexes(t_data *data);
 
 void	prepare_threads(t_data *data)
 {
 	int	philo;
 
 	philo = 0;
-	pthread_mutex_init(data->need_stop_mutex, NULL);
-	pthread_mutex_init(data->start_time_mutex, NULL);
-	pthread_mutex_init(data->philo_dones_mutex, NULL);
-	while ((philo < data->config->number_of_philosophers))
-	{
-		pthread_mutex_init(data->forks[philo]->mutex, NULL);
-		philo++;
-	}
-	philo = 0;
-	get_start_time(data);
+	prepare_mutexes(data);
+	data->start_time = get_time_now();
 	pthread_create(data->observer_thread, NULL, &observer_routine, data);
 	while ((philo < data->config->number_of_philosophers))
 	{
-		pthread_mutex_init(data->philosophers[philo]->shared->last_meal_mutex, NULL);
-		pthread_create(data->philo_threads[philo], NULL, &philosopher_routine, data->philosophers[philo]);
+		pthread_create(data->philo_threads[philo],
+			NULL, &philosopher_routine, data->philosophers[philo]);
 		philo++;
 	}
 	philo = 0;
@@ -49,5 +34,22 @@ void	prepare_threads(t_data *data)
 	{
 		pthread_join((*data->philo_threads[philo]), NULL);
 		philo++;
+	}
+}
+
+static void	prepare_mutexes(t_data *data)
+{
+	int	index;
+
+	index = 0;
+	pthread_mutex_init(data->need_stop_mutex, NULL);
+	pthread_mutex_init(data->start_time_mutex, NULL);
+	pthread_mutex_init(data->philo_dones_mutex, NULL);
+	while ((index < data->config->number_of_philosophers))
+	{
+		pthread_mutex_init(data->forks[index]->mutex, NULL);
+		pthread_mutex_init(data->philosophers[index]->shared->last_meal_mutex,
+			NULL);
+		index++;
 	}
 }
