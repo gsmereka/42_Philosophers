308fa64d1fef4409a673e059fbfeaabc5478f755
gsmereka <gabrielsmereka200@gmail.com>
2023-05-10 17:29:24 -0300

    att prepare_threads

--- philo/src/prepare_threads.c
+++ philo/src/prepare_threads.c
@@ -6,7 +6,7 @@
 /*   By: gsmereka <gsmereka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 14:07:13 by gsmereka          #+#    #+#             */
-/*   Updated: 2023/05/10 17:18:09 by gsmereka         ###   ########.fr       */
+/*   Updated: 2023/05/10 17:21:51 by gsmereka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,25 @@ static void	prepare_mutexes(t_data *data);
 
 void	prepare_threads(t_data *data)
 {
-	int	philo;
+	int	index;
+	int	thread_join_index;
 
-	philo = 0;
+	index = 0;
+	thread_join_index = 0;
 	prepare_mutexes(data);
 	data->start_time = get_time_now();
 	pthread_create(data->observer_thread, NULL, &observer_routine, data);
-	while ((philo < data->config->number_of_philosophers))
+	while ((index < data->config->number_of_philosophers))
 	{
-		pthread_create(data->philo_threads[philo],
-			NULL, &philosopher_routine, data->philosophers[philo]);
-		philo++;
+		pthread_create(data->philo_threads[index],
+			NULL, &philosopher_routine, data->philosophers[index]);
+		index++;
 	}
-	philo = 0;
 	pthread_join((*data->observer_thread), NULL);
-	while ((philo < data->config->number_of_philosophers))
+	while ((thread_join_index < data->config->number_of_philosophers))
 	{
-		pthread_join((*data->philo_threads[philo]), NULL);
-		philo++;
+		pthread_join((*data->philo_threads[thread_join_index]), NULL);
+		thread_join_index++;
 	}
 }
 
